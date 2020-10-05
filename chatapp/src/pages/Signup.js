f2b7bd738d15cb52e227f3482e48e9cca3f349af
unknown <soreiyu52@gmail.com>
2020-10-05 21:27:07 +0900

    サインアップページの基本フォームの作成

--- chatapp/src/pages/Signup.js
+++ chatapp/src/pages/Signup.js
@@ -2,9 +2,34 @@ import React from 'react'
 
 const SignUp = () => {
     return (
-        <>
-            <h1>SignUp</h1>
-        </>
+        <div>
+            <h1>Sign Up</h1>
+            <form>
+                <div>
+                    <label htmlFor='email'>E-mail</label>
+                    <input
+                        name='email'
+                        type='email'
+                        // getElementByIdで取得するID
+                        id='email'
+                        // 初期値'Email'を入れる
+                        placeholder='Email'
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        name='password'
+                        type='password'
+                        // getElementByIdで取得するID
+                        id='password'
+                        // 初期値'password'を入れるZ
+                        placeholder='Password'
+                    />
+                </div>
+                <button type='submit'>Sign Up</button>
+            </form>
+        </div>
     )
 }
 
