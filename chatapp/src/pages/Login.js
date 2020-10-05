dfca8e7a43472e7f73805df12310ee2b541dccdf
unknown <soreiyu52@gmail.com>
2020-10-05 21:15:21 +0900

    ログインページ基本フォームの作成

--- chatapp/src/pages/Login.js
+++ chatapp/src/pages/Login.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
     return (
         <>
             <h1>Login</h1>
+            <form>
+                <div>
+                    <label htmlFor='email'>E-mail</label>
+                    <input
+                        type='email'
+                        // getElementByIdで取得するID
+                        id='email'
+                        name='email'
+                        // 初期値'Email'を入れる
+                        placeholder='Email'
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        type='password'
+                        // getElementByIdで取得するID
+                        id='password'
+                        name='password'
+                        // 初期値'password'を入れる
+                        placeholder='password'
+                    />
+                </div>
+                <button type='submit'>Login</button>
+            </form>
         </>
     )
 }
